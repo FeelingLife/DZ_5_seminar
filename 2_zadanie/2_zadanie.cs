cc876b783f13821b67be17397628fc215ba2f50a
Nikita <crazefire@mail.ru>
2023-04-23 22:11:31 +0300

    2 zadanie complete

--- 2_zadanie/2_zadanie.cs
+++ 2_zadanie/2_zadanie.cs
@@ -5,3 +5,15 @@
 
 // [-4, -6, 89, 6] -> 0
 
+int[] array = new int[6];
+
+for (int i = 0; i < array.Length; i++)
+{
+    int randomNumber = new Random().Next(1, 10);
+    array[i] = randomNumber;
+    Console.Write(array[i] + " ");
+    Console.Write(" ");
+
+}
+int sumNumbers = array[1] + array[3] + array[5];
+Console.WriteLine($"Сумма элементов, стоящих на нечётных индексах: {sumNumbers}");
