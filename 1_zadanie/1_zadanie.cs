71720af09169cf8d66b626815f711cf61d2a0972
Nikita <crazefire@mail.ru>
2023-04-23 21:52:47 +0300

    getrg

---
+++ 1_zadanie/1_zadanie.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами (длина массива 5 элементов). 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[5];
+int sumNumbers = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    int randomNumber = new Random().Next(100, 1000);
+    array[i] = randomNumber;
+    Console.Write(array[i] + " ");
+    Console.WriteLine(" ");
+}
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sumNumbers = sumNumbers + 1;
+            Console.WriteLine($"Количество четных чисел: {sumNumbers}");
+        }
+    }
